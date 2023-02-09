3713a2269de0bb242c0cb406ed2e96614e527324
spangen87 <rikard.spangmyr@bestlas.se>
2023-02-09 14:00:44 +0000

    feat: Update model to choices instead of boolean and render out all field in template

--- door_automation_forms/models.py
+++ door_automation_forms/models.py
@@ -46,37 +46,43 @@ class ControlChart(models.Model):
         ('2-flyg', '2-flyg'),
     ]
 
+    CHOICES = [
+        ('OK', 'OK'),
+        ('ANM', 'Anm.'),
+        ('N/A', 'Ej aktuell'),
+    ]
+
     object = models.ForeignKey('Object', on_delete=models.CASCADE)
     door_type = models.CharField(max_length=6, choices=DOOR_TYPE_CHOICES, verbose_name='Dörrtyp')
     position_id = models.CharField(max_length=14, verbose_name='Dörrposition/ID')
     done_by = models.CharField(max_length=54, verbose_name='Utfört av')
     date = models.DateField()
 
-    doorframe = models.BooleanField(verbose_name='Dörrkarm')
-    door_leaves = models.BooleanField(verbose_name='Dörrblad')
-    threshold = models.BooleanField(verbose_name='Tröskel')
-    hinge = models.BooleanField(verbose_name='Gångjärn')
-    lock_chest = models.BooleanField(verbose_name='Låskista')
-    end_plate = models.BooleanField(verbose_name='Slutbleck')
-    edge_rule = models.BooleanField(verbose_name='Kantregel')
-    door_coordinator = models.BooleanField(verbose_name='Dörrkoordinator')
-    door_environment_clean = models.BooleanField(verbose_name='Dörrmiljö rengjord')
+    doorframe = models.CharField(max_length=6, choices=CHOICES, verbose_name='Dörrkarm')
+    door_leaves = models.CharField(max_length=6, choices=CHOICES, verbose_name='Dörrblad')
+    threshold = models.CharField(max_length=6, choices=CHOICES, verbose_name='Tröskel')
+    hinge = models.CharField(max_length=6, choices=CHOICES, verbose_name='Gångjärn')
+    lock_chest = models.CharField(max_length=6, choices=CHOICES, verbose_name='Låskista')
+    end_plate = models.CharField(max_length=6, choices=CHOICES, verbose_name='Slutbleck')
+    edge_rule = models.CharField(max_length=6, choices=CHOICES, verbose_name='Kantregel')
+    door_coordinator = models.CharField(max_length=6, choices=CHOICES, verbose_name='Dörrkoordinator')
+    door_environment_clean = models.CharField(max_length=6, choices=CHOICES, verbose_name='Dörrmiljö rengjord')
 
-    opener_attached_to_the_wall = models.BooleanField(verbose_name='Öppnare riktigt fäst till vägg/parti')
-    arm_system_attached = models.BooleanField(verbose_name='Armsystem riktigt fäst till öppnare/dörr')
-    arm_system_adjusted = models.BooleanField(verbose_name='Armsystem riktigt justerat')
-    cloosing_speed_adjusted1 = models.BooleanField(verbose_name='Stängningshastighet riktigt justerat 1-flyg')
-    closing_speed_adjusted2 = models.BooleanField(verbose_name='Stängningshastighet riktigt justerat 2-flyg')
-    opening_speed_adjusted1 = models.BooleanField(verbose_name='Öppningshastighet riktigt justerat 1-flyg')
-    opening_speed_adjusted2 = models.BooleanField(verbose_name='Öppningshastighet riktigt justerat 2-flyg')
-    opening_coordination = models.BooleanField(verbose_name='Öppningskoordinering riktigt inställt 2-flyg')
+    opener_attached_to_the_wall = models.CharField(max_length=6, choices=CHOICES, verbose_name='Öppnare riktigt fäst till vägg/parti')
+    arm_system_attached = models.CharField(max_length=6, choices=CHOICES, verbose_name='Armsystem riktigt fäst till öppnare/dörr')
+    arm_system_adjusted = models.CharField(max_length=6, choices=CHOICES, verbose_name='Armsystem riktigt justerat')
+    cloosing_speed_adjusted1 = models.CharField(max_length=6, choices=CHOICES, verbose_name='Stängningshastighet riktigt justerat 1-flyg')
+    closing_speed_adjusted2 = models.CharField(max_length=6, choices=CHOICES, verbose_name='Stängningshastighet riktigt justerat 2-flyg')
+    opening_speed_adjusted1 = models.CharField(max_length=6, choices=CHOICES, verbose_name='Öppningshastighet riktigt justerat 1-flyg')
+    opening_speed_adjusted2 = models.CharField(max_length=6, choices=CHOICES, verbose_name='Öppningshastighet riktigt justerat 2-flyg')
+    opening_coordination = models.CharField(max_length=6, choices=CHOICES, verbose_name='Öppningskoordinering riktigt inställt 2-flyg')
 
-    safety_sensor_tested = models.BooleanField(verbose_name='Säkerhetssensor, justerad och testad')
-    impulse_sensor_adjusted = models.BooleanField(verbose_name='Impulsgivare (radar) riktigt monterat och justerat')
-    control_signals_tested = models.BooleanField(verbose_name='Styrsignaler från passersystem testat')
-    fire_signals_tested = models.BooleanField(verbose_name='Styrsignaler från brandlarm testat')
+    safety_sensor_tested = models.CharField(max_length=6, choices=CHOICES, verbose_name='Säkerhetssensor, justerad och testad')
+    impulse_sensor_adjusted = models.CharField(max_length=6, choices=CHOICES, verbose_name='Impulsgivare (radar) riktigt monterat och justerat')
+    control_signals_tested = models.CharField(max_length=6, choices=CHOICES, verbose_name='Styrsignaler från passersystem testat')
+    fire_signals_tested = models.CharField(max_length=6, choices=CHOICES, verbose_name='Styrsignaler från brandlarm testat')
 
-    notes = models.TextField(max_length=1000, verbose_name='Anmärkningar', help_text='Ange anmärkningar till fälten som inte är ibockade ovan')
+    notes = models.TextField(max_length=1000, verbose_name='Anmärkningar', help_text='Ange anmärkningar. Börja med siffran följt av notering. Ny anmärkning på ny rad.')
 
     signature = JSignatureField()
 
